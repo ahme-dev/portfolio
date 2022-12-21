9afb9e4c4889869358d9d8d17219746984537db8
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-21 18:40:59 +0300

    Style the Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,31 +1,47 @@
 export function Home() {
 	return (
-		<div className="flex flex-col gap-4 ">
+		<div className="flex flex-col justify-between h-[90vh]">
 			{/* Top */}
 			<header className="flex justify-between gap-4">
 				{/* Logo */}
-				<div>
-					<h1>logo</h1>
+				<div className="bg-teal-100 rounded-3xl px-5 py-3">
+					<h1 className="text-neutral-900 font-bold">logo</h1>
 				</div>
 				{/* Icons */}
-				<div className="flex gap-2">
-					<p>1</p>
-					<p>2</p>
-					<p>3</p>
+				<div className="flex gap-2 bg-teal-100 rounded-3xl px-5 py-3">
+					<p className="text-neutral-900 font-bold">1</p>
+					<p className="text-neutral-900 font-bold">2</p>
+					<p className="text-neutral-900 font-bold">3</p>
 				</div>
 			</header>
 
 			{/* Middle About */}
-			<div className="self-center">
-				Hi, my name's Ahmed. Lorem ipsum dolor sit amet consectetur, adipisicing
-				elit. Consectetur delectus omnis dolor rem quae fuga odio sint placeat!
-			</div>
+			<p className="self-center max-w-lg font-bold text-2xl">
+				Hi, my name's <b className="text-teal-100">Ahmed</b>. <br /> Lorem ipsum
+				dolor sit amet consectetur, adipisicing elit. Consectetur delectus omnis
+				dolor rem quae fuga odio sint placeat!
+			</p>
 
 			{/* Bottom Nav */}
-			<nav className="self-center flex gap-4">
-				<a href="">link</a>
-				<a href="">link</a>
-				<a href="">link</a>
+			<nav className="self-center flex items-center gap-0 bg-teal-100 text-neutral-900 font-bold rounded-3xl p-1">
+				<a
+					href="#"
+					className="hover:bg-neutral-900 hover:text-teal-100 px-4 py-2 rounded-3xl"
+				>
+					Projects
+				</a>
+				<a
+					href="#"
+					className="hover:bg-neutral-900 hover:text-teal-100 px-4 py-2 rounded-3xl"
+				>
+					Skills
+				</a>
+				<a
+					href="#"
+					className="hover:bg-neutral-900 hover:text-teal-100 px-4 py-2 rounded-3xl"
+				>
+					Contact
+				</a>
 			</nav>
 		</div>
 	);
