fb3d8653993e316a7691f43421e7153c1e250d69
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-21 22:04:51 +0300

    Move padding to app, not components

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Skills } from "./components/Skills";
 export default function App() {
 	return (
 		<div className="bg-neutral-900 text-neutral-100 min-h-screen">
-			<div className="flex flex-col gap-32 [&>*]:md:px-32 [&>*]:px-6">
+			<div className="flex flex-col gap-32 md:px-32 px-6">
 				<Home></Home>
 				<Projects></Projects>
 				<Skills></Skills>
