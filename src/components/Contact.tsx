078aed142984a6c26d0267e4f36bc8d6fb2678ef
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-21 20:26:08 +0300

    Style the Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,77 @@
+import { useState } from "react";
+
 export function Contact() {
+	const [form, setForm] = useState({
+		name: "",
+		email: "",
+		message: "",
+	});
+
 	return (
-		<div className="flex flex-col md:grid md:grid-cols-2 gap-4">
+		<div className="flex flex-col md:grid md:grid-cols-2 gap-8">
 			{/* Left */}
-			<div className="flex flex-col gap-4 p-8 bg-teal-100 rounded-3xl">
-				<input placeholder="Name" className="bg-transparent" type="text" />
-				<input placeholder="Email" className="bg-transparent" type="text" />
-				<textarea placeholder="Message" className="bg-transparent" />
-			</div>
+			<form className="flex flex-col gap-6 p-8 bg-teal-100 text-neutral-900 rounded-3xl">
+				<div className="flex flex-col md:flex-row gap-2 border-2 rounded-3xl px-4 py-2 border-neutral-900 hover:bg-neutral-900 hover:text-neutral-100 focus-within:bg-neutral-900 focus-within:text-neutral-100">
+					<label className="font-bold" htmlFor="name">
+						Name
+					</label>
+					<input
+						name="name"
+						value={form.name}
+						onChange={(e) =>
+							setForm((form) => ({ ...form, name: e.target.value }))
+						}
+						className="bg-transparent w-full focus:outline-none"
+						type="text"
+					/>
+				</div>
+				<div className="flex flex-col md:flex-row gap-2 border-2 rounded-3xl px-4 py-2 border-neutral-900 hover:bg-neutral-900 hover:text-neutral-100 focus-within:bg-neutral-900 focus-within:text-neutral-100">
+					<label className="font-bold" htmlFor="email">
+						Email
+					</label>
+					<input
+						name="email"
+						required
+						value={form.email}
+						onChange={(e) =>
+							setForm((form) => (form = { ...form, email: e.target.value }))
+						}
+						className="bg-transparent w-full focus:outline-none"
+						type="email"
+					/>
+				</div>
+				<div className="flex flex-col md:flex-row gap-2 border-2 rounded-3xl px-4 py-2 border-neutral-900 hover:bg-neutral-900 hover:text-neutral-100 focus-within:bg-neutral-900 focus-within:text-neutral-100">
+					<label className="font-bold" htmlFor="message">
+						Message
+					</label>
+					<textarea
+						value={form.message}
+						onChange={(e) =>
+							setForm((form) => (form = { ...form, message: e.target.value }))
+						}
+						name="message"
+						maxLength={250}
+						className="bg-transparent w-full focus:outline-none max-h-24"
+					/>
+				</div>
+				<button className="rounded-3xl px-4 py-2 font-bold border-2 border-neutral-900 hover:bg-neutral-900 hover:text-neutral-100 w-fit self-end focus:text-neutral-100 focus:outline-none focus:bg-neutral-900">
+					Send
+				</button>
+			</form>
 
 			{/* Right */}
-			<div className="p-8">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae
-				aliquid, blanditiis provident itaque voluptatibus voluptatem rem illo
-				animi similique, repellendus deserunt magni quasi tenetur delectus
-				dignissimos cum, saepe alias molestias nihil. Ducimus, dolore.
+			<div className="flex flex-col justify-center text-lg gap-4 p-8">
+				<p>{form.email}</p>
+				<p>
+					Hi, <b>Ahmed</b>
+				</p>
+				<p className="overflow-y-scroll overflow-x-scroll">
+					{form.message || "Nothing to say."}
+				</p>
+				<p>
+					Regards, <br />
+					<b>{form.name || "Unknown"}</b>
+				</p>
 			</div>
 		</div>
 	);
