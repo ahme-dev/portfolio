699bbade0d53859d75d56a7595c1316dc3b0744c
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-22 19:36:06 +0300

    Add plusing background blur

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ import { Skills } from "./components/Skills";
 export default function App() {
 	return (
 		<div className="bg-neutral-900 text-neutral-100 min-h-screen">
-			<div className="flex flex-col gap-32 md:px-32 px-6">
+			{/* Bubble */}
+			<div className="bg-teal-100/50 w-32 h-32 fixed top-1/3 left-1/2 z-0 filter blur-[50rem] animate-pulse"></div>
+			{/* App Components */}
+			<div className="flex flex-col gap-32 md:px-32 px-6 [&>*]:z-10">
 				<Home></Home>
 				<Projects></Projects>
 				<Skills></Skills>
