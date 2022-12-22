2efbb3cc4872521c80e39016b02ac2f54d9b2c14
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-22 21:18:24 +0300

    Add another bg image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import WaveImg from "./assets/wave.svg";
+import ScatterImg from "./assets/scatter.svg";
 
 export default function App() {
 	return (
 		<div className="bg-neutral-900 text-neutral-100 min-h-screen">
-			{/* Waves */}
+			{/* Background */}
 			<img
 				className="absolute w-screen left-0 top-0 opacity-5 z-0"
 				src={WaveImg}
+			/>
+			<img
+				className="fixed w-screen left-0 top-8 opacity-5 z-0"
+				src={ScatterImg}
 			/>
 			{/* App Components */}
 			<div className="flex flex-col gap-32 md:px-32 px-6 [&>*]:z-10">
@@ -19,7 +23,6 @@ export default function App() {
 				<Projects></Projects>
 				<Skills></Skills>
 				<Contact></Contact>
-				<Footer></Footer>
 			</div>
 		</div>
 	);
