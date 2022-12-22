a84db5998bf8a3c1cef4d91c13cb02a63eafa6d9
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-22 20:10:58 +0300

    Restyle the Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,44 +4,21 @@ export function Home() {
 	return (
 		<div
 			id="Home"
-			className="flex flex-col justify-between min-h-screen py-4 md:py-8"
+			className="flex flex-col md:flex-row items-center justify-center md:justify-between min-h-screen py-4 md:py-8 gap-16"
 		>
-			{/* Top */}
-			<header className="flex justify-between gap-4 text-neutral-900">
-				{/* Logo */}
-				<div className="bg-teal-100 rounded-3xl px-5 py-3">
-					<h1 className="font-bold">LOGO</h1>
-				</div>
-				{/* Icons */}
-				<div className="flex gap-2 bg-teal-100 rounded-3xl h-10 p-1">
-					<SiProtonmail
-						href="#"
-						className="w-full h-full hover:bg-neutral-900 hover:text-teal-100 rounded-full p-1 cursor-pointer transition-all"
-					></SiProtonmail>
-					<SiGithub
-						href="#"
-						className="w-full h-full hover:bg-neutral-900 hover:text-teal-100 rounded-full p-1 cursor-pointer transition-all"
-					></SiGithub>
-					<SiTelegram
-						href="#"
-						className="w-full h-full hover:bg-neutral-900 hover:text-teal-100 rounded-full p-1 cursor-pointer transition-all"
-					></SiTelegram>
-				</div>
-			</header>
-
-			{/* Middle About */}
+			{/* Text */}
 			<p className="self-center max-w-lg font-bold text-2xl">
 				Hi, my name's <b className="text-teal-100">Ahmed</b>. <br /> Lorem ipsum
 				dolor sit amet consectetur, adipisicing elit. Consectetur delectus omnis
 				dolor rem quae fuga odio sint placeat!
 			</p>
 
-			{/* Bottom Nav */}
-			<nav className="self-center flex items-center gap-0 bg-teal-100 text-neutral-900 font-bold rounded-3xl p-1">
+			{/* Nav */}
+			<nav className="flex flex-row gap-4 flex-wrap items-center justify-center w-full">
 				{["Projects", "Skills", "Contact"].map((el) => (
 					<a
 						href={`#${el}`}
-						className="hover:bg-neutral-900 hover:text-teal-100 px-4 py-2 rounded-3xl transition-all"
+						className="bg-teal-100/10 hover:bg-teal-100 hover:text-neutral-900 font-bold h-32 w-32 rounded-3xl grid place-items-center filter transition-all"
 					>
 						{el.toUpperCase()}
 					</a>
