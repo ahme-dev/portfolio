81005b80b94cc5d0111ae84e3aff2374faa5e923
Ahmed K.A <ahmadkabdullah@pm.me>
2022-12-22 20:11:24 +0300

    Add waves bg and remove bubble from App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
+import WaveImg from "./assets/wave.svg";
 
 export default function App() {
 	return (
 		<div className="bg-neutral-900 text-neutral-100 min-h-screen">
-			{/* Bubble */}
-			<div className="bg-teal-100/50 w-32 h-32 fixed top-1/3 left-1/2 z-0 filter blur-[50rem] animate-pulse"></div>
+			{/* Waves */}
+			<img
+				className="absolute w-screen left-0 top-0 opacity-5 z-0"
+				src={WaveImg}
+			/>
 			{/* App Components */}
 			<div className="flex flex-col gap-32 md:px-32 px-6 [&>*]:z-10">
 				<Home></Home>
