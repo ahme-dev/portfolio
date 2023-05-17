9c84a8365e898d66ef8d15505d93d1dc485f6c1f
Ahmed K. A <ahmadkabdullah@pm.me>
2023-05-17 14:48:41 +0300

    Integrate with new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import compress from 'astro-compress'
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [compress(), mdx(), tailwind()],
+})
